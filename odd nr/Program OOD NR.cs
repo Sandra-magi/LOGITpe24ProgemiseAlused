1e3e26534b2ca26f149b52c842e8ec2845bd1841
Opilane TTHK <opilane@tthk.ee>
2025-03-04 11:12:26 +0200

    prograd

---
+++ odd nr/Program OOD NR.cs
@@ -0,0 +1,32 @@
+﻿namespace odd_nr
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            {
+                Console.Write("Sisesta number: ");
+                string kasutajaSisend = Console.ReadLine();
+
+                if (int.TryParse(kasutajaSisend, out int number))
+                {
+                    if (number % 2 == 0)
+                    {
+                        Console.WriteLine($"Number {number} on paaris.");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Number {number} on paaritu.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Sisestasite vale numbri!");
+                }
+            }
+        }
+
+    }
+}
+
