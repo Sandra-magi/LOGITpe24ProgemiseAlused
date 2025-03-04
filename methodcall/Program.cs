18db5cabf4141950c4c1d0342268224e13d2ab0c
Opilane TTHK <sandramagi0504@gmail.com>
2025-03-04 11:26:30 +0200

    methoodcallll

---
+++ methodcall/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace methodcall
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Jalgratas();
+
+            static void Jalgratas()
+                {
+                Console.WriteLine("Jalgratta meetodis");
+            }
+        }
+    }
+}
