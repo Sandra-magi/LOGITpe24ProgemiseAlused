4e92cca881d08486192dc3ac4a75dfe289e81121
Opilane TTHK <sandramagi0504@gmail.com>
2025-03-06 09:52:06 +0200

    rando colour

---
+++ randocolor/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Drawing;
+
+namespace randocolor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("random colour");
+
+            Colour c = (Colour)(new Random()).Next(0, 5);
+        
+            for (int i = 0; i < 5; i++)
+            {
+                switch (c)
+            }
+            
+
+
+            public enum colour
+            {
+                Red,
+                Green,
+                Blue,
+                Pink,
+                Purple,
+                White,
+            }
+    }   switch (c)
+    
+    case Colour.Red:
+        Console.BackgroundColor = ConsoleColor.Red;
+        Console.Clear();
+        Console.WriteLine("Red");
+        break; 
+
+        default:
+            Console.WriteLine("error");
+             break;
+} 
