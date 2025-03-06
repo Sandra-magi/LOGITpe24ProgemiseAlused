e2094b5a2d5fc0bc3fa68fb5d1cd77bd68ac4a99
Opilane TTHK <sandramagi0504@gmail.com>
2025-03-06 09:05:36 +0200

    täring

---
+++ randoms/Program.cs
@@ -0,0 +1,61 @@
+﻿namespace randoms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            int number = new Random().Next(1, 7); 
+
+            switch (number)
+
+            case 1:
+                Console.WriteLine("veeretasid nr " + number);
+                break;
+
+                default:
+                Console.WriteLine("error");
+                break;
+
+            case 2:
+                Console.WriteLine("veeretasid nr " + number);
+                break;
+
+            default:
+                Console.WriteLine("error");
+                break;
+
+            case 3:
+                Console.WriteLine("veeretasid nr " + number);
+                break;
+
+            default:
+                Console.WriteLine("error");
+                break;
+
+            case 4:
+                Console.WriteLine("veeretasid nr " + number);
+                break;
+
+            default:
+                Console.WriteLine("error");
+                break;
+
+            case 5:
+                Console.WriteLine("veeretasid nr " + number);
+                break;
+
+            default:
+                Console.WriteLine("error");
+                break;
+
+            case 6:
+                Console.WriteLine("veeretasid nr " + number);
+                break;
+
+            default:
+                Console.WriteLine("error");
+                break;
+            }
+}
